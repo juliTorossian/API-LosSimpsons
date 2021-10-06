08e91a839f7243e887b7bf3c2ed86844045a71d4
juliTorossian <72228363+juliTorossian@users.noreply.github.com>
2021-10-06 13:59:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,57 +6,53 @@ import os
 
 app = Flask('__name__')
 
-respuesta = Response()
-fraseMod = FraseMod()
-respuesta.headers['Access-Control-Allow-Origin'] = '*'
 
 #*#*#*#*#* /frases *#*#*#*#*#
 
 @app.route('/frases', methods=['GET'])
 def get_frases():
-    global respuesta
     respuesta = jsonify(FraseMod.buscaFrases())
+    respuesta.headers['Access-Control-Allow-Origin'] = '*'
     return respuesta
 
 @app.route('/frase', methods=['GET'])
 def get_frase_random():
-    global respuesta
-    global fraseMod
-    respuesta = jsonify(fraseMod.buscaFraseRandom())
+    respuesta = jsonify(FraseMod.buscaFraseRandom())
+    respuesta.headers['Access-Control-Allow-Origin'] = '*'
     return respuesta
 
 @app.route('/frase/<int:fraseId>', methods=['GET'])
 def get_frase_id(fraseId):
-    global respuesta
     respuesta = jsonify(FraseMod.buscaFraseId(fraseId))
+    respuesta.headers['Access-Control-Allow-Origin'] = '*'
     return respuesta
 
 @app.route('/frase/personaje/<int:personajeId>', methods=['GET'])
 def get_frase_personaje(personajeId):
-    global respuesta
     respuesta = jsonify(FraseMod.buscaFrasePersonaje(personajeId))
+    respuesta.headers['Access-Control-Allow-Origin'] = '*'
     return respuesta
 
 #*#*#*#*#* /personajes *#*#*#*#*#
 
 @app.route('/personajes', methods=['GET'])
 def get_persoanjes():
-    global respuesta
     respuesta = jsonify(FraseMod.buscarPerosnajes())
+    respuesta.headers['Access-Control-Allow-Origin'] = '*'
     return respuesta
 
 #*#*#*#*#* /capitulos *#*#*#*#*#
 
 @app.route('/capitulos/temporada/<int:tempNro>', methods=['GET'])
 def get_caps_temporada(tempNro):
-    global respuesta
     respuesta = jsonify(FraseMod.capitulosPorTemporada(tempNro))
+    respuesta.headers['Access-Control-Allow-Origin'] = '*'
     return respuesta
 
 @app.route('/capitulo/random', methods=['GET'])
 def get_cap_random():
-    global respuesta
     respuesta = jsonify(FraseMod.capituloRandom())
+    respuesta.headers['Access-Control-Allow-Origin'] = '*'
     return respuesta
 
 
